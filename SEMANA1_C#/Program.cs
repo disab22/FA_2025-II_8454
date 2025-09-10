9e249194e7d3bc16c2595ea7f9a87049899a886d
disab17 <cordovaallison83@gmail.com>
2025-09-10 12:52:40 -0500

    subiendo hasta el ejer5 sem3

--- SEMANA1_C#/Program.cs
+++ SEMANA1_C#/Program.cs
@@ -12,7 +12,7 @@ namespace S01_C_
         static void Main(string[] args)
 
         {
-            EJER_2();
+            EJER_5();
             Console.ReadKey();
         }
         static void EJER_1()
@@ -29,36 +29,55 @@ namespace S01_C_
         static void EJER_2()
 
         {
-            String nombre;
-            nombre= "Mia"
-            Console.WriteLine("Su nombre es {nombre}");
-            
-
-
+           Console.WriteLine("\"Allison\"");
         }
+
         static void EJER_3()
 
         {
+            Console.Write("Ingrese numero 1:");
+            int num1=int.Parse(Console.ReadLine());
 
+            Console.Write("Ingrese numero 2:");
+            int num2=int.Parse(Console.ReadLine());
 
+            double divi = (double)num1 / (double)num2;
 
+            Console.WriteLine("\nSuma: " + (num1 + num2));
+            Console.WriteLine("Resta: " + (num1 + num2));
+            Console.WriteLine("Multiplicacion: " + (num1 * num2));
+            Console.WriteLine("Division: " + divi);
         }
+
         static void EJER_4()
 
         {
+            Console.Write("Ingrese un numero decimal: ");
+            double num=double.Parse(Console.ReadLine());    
 
+            double raiz2=Math.Sqrt(num);
+            double redo=Math.Round(num,0);
+            double cubo=Math.Pow(num,3);
+            double raiz3=Math.Pow(num,1/3d);
 
-
+            Console.WriteLine("\nRaiz 2: " +raiz2);
+            Console.WriteLine("Redondeando:"+redo);
+            Console.WriteLine("Al cubo:" + cubo);
+            Console.WriteLine("Raiz cubica:" + raiz3);
         }
+
         static void EJER_5()
 
         {
+            Console.Write("Ingrese un numero: ");
+            string num=Console.ReadLine();
 
+            int entero=int.Parse(num);  
+            double deci=double.Parse(num);
 
-
+            Console.WriteLine("\nResto: "+(entero % 2));
+            Console.WriteLine("Dividido 3: " + (deci / 3));
         }
-
-
 
         static void EJER_6()
 
--- SEMANA1_PYTHON/SEMANA1_PYTHON.py
+++ SEMANA1_PYTHON/SEMANA1_PYTHON.py
@@ -1,8 +1,49 @@
+import math
+
+
 def ejer1():
     nombre = input("Ingresa tu nombre:")
     carrera = input("Ingresa tu carrera:")
 
     print(f"\n{nombre},  Bienvenido al curso de Fundamentos de Algoritmos de la carrera {carrera}")
 
-ejer1() 
+def ejer2():
+    print("\"Allison\"")
+
+def ejer3():
+    N1=int(input("Ingrese N1: "))
+    N2=int(input("Ingrese N2: "))
+
+    print("\nSuma: ", (N1+N2))
+    print("Reta: ", (N1-N2))
+    print("Multi: ", (N1*N2))
+    print ("Divi: ",(N1/N2))
+
+def ejer4():
+    num=float(input("Ingrese un numero decimal: "))
+
+    raiz2 = math.sqrt(num)
+    redo = round(num,0)
+    cubo = math.pow(num,3)
+    raiz3 = math.pow(num,1/3)
+
+    print("\nRaiz 2: ",raiz2)
+    print("Redondeado: ", redo)
+    print("Al cubo: ", cubo)
+    print("Raiz3: ",raiz3)
+
+def ejer5():
+    num = input("Ingrese un numero: ")
+
+    entero = int(num)
+    deci = float(num)
+
+    print("\nResto: ", entero%2)
+    print("\nDividido 3: ", deci/3)
+
+
+
+
+
+ejer4() 
 
