840d2b88edc086625bdcf5c5279c56cb05dcba86
disab22 <disab738@gmail.com>
2025-09-17 12:17:33 -0600

    semana5

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,10 @@
+def ejer1():
+    edad= int(input("Ingrese su edad:"))
+
+    if edad>18 
+        print("Menor de edad")
+    else:
+        if edad <=64
+            print("Adulto")
+        else
+            print("Adulto mayorb ")
